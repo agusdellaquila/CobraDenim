06ab4d3aebfe631a8c76d89d8a5339deee0dd49a
agusdellaquila <agusdellaquila72@gmail.com>
2022-01-31 00:09:23 -0300

    Aplicando arrays

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 //----------------- Funciones ----------------
-function cartTotal(item1, item2, item3) {
-    //retonra el total de los items en el carrito, seria mejor pasarle un array donde se encuentren los items en carrito
-    // y que los sume, pero por el momento con variables
-    return (item1 + item2 + item3);
-}
-
 function applyDiscount(total, descuento) {
     total = total - ((total * descuento) / 100);
     return total;
@@ -21,11 +15,39 @@ function applyDues(total, duePercentaje , cantDues) {
     return (total/cantDues)
 }
 
-//-------------------Simulador-------------------------
-let cartItem1 = 5000;
-let cartItem2 = 2000;
-let cartItem3 = 3000;
-let totalCarrito;
+//objetos
+class Prenda {
+    constructor(nroItem, precio, stockTotal, stockEnCarro, estadoEnCarro, dineroTotalEnCarro) {
+        //propiedades del objeto
+        this.nroItem = nroItem;
+        this.precio = precio;
+        this.stockTotal = stockTotal; 
+        this.stockEnCarro = stockEnCarro;
+        this.estadoEnCarro = estadoEnCarro;
+        this.dineroTotalEnCarro = dineroTotalEnCarro;
+    }
+    mostrar() {
+
+        console.log('El producto ' + this.nroItem + ' tiene un precio de ' + this.precio + ' y un stock de ' + this.stockTotal);
+    }
+    valorDeMercaderia () {
+        let valorTotalDeMercaderia;
+        valorTotalDeMercaderia = this.precio * this.stockTotal;
+        console.log('El valor total invertido en el item ' + this.nroItem + ' es de ' + valorTotalDeMercaderia + '$')
+        return valorTotalDeMercaderia
+    }
+
+    valorDeMercaderiaEnCarro () {
+        this.dineroTotalEnCarro = this.precio * this.stockEnCarro;
+        return this.dineroTotalEnCarro
+    }
+}
+
+
+//-------------------declaraciones-------------------------
+let carrito = [];
+
+let totalCarrito = 0;
 let palabraDescuento = '';
 let palabraCuotas = '';
 let valorDescuento;
@@ -33,9 +55,72 @@ let cantCuotas;
 let porcentajeCuotas = 20;
 let valorDeCuota;
 
+//creacion de objeos
+let item1 = new Prenda(1, 5000, 10, 0, false, 0);
+let item2 = new Prenda(2, 3500, 13, 0, false, 0);
+let item3 = new Prenda(3, 7000, 5, 0, false, 0);
+
+//carga de carro
+carrito.push(item1, item2, item3);
+
 // INTERFAZ EN CONSOLE
-totalCarrito = cartTotal(cartItem1, cartItem2, cartItem3);
-console.log('Su total es de ' + totalCarrito);
+//--------------------incorporacion de objetos---------------------------
+let flag = false;
+while (!flag) {
+    let agregarAlCarro = Number(prompt('Que item quiere agregar al carro? (1-2-3): '))
+    let CantidadAgregarAlCarro = Number(prompt('Que cantidad del item ' + agregarAlCarro + ' quiere agregar? : '))
+
+    if (agregarAlCarro == 1) { //esto dentro de este if se repite 3 veces, podria estar en una funcion que probablemente implemente mas adelante
+
+        if ((carrito[0].stockTotal > 0) && (carrito[0].stockTotal >= CantidadAgregarAlCarro)) {
+            carrito[0].stockTotal = carrito[0].stockTotal - CantidadAgregarAlCarro;
+            carrito[0].enCarro = true;
+            carrito[0].stockEnCarro = carrito[0].stockEnCarro + CantidadAgregarAlCarro;
+            // carrito[0].dineroTotalEnCarro = carrito[0].precio * carrito[0].stockEnCarro;
+        } 
+        else {
+            console.log('Lo sentimos no queda suficiente stock de este producto');
+        }
+    } else if (agregarAlCarro == 2) {
+        if ((carrito[1].stockTotal > 1) && (carrito[1].stockTotal >= CantidadAgregarAlCarro)) {
+            carrito[1].stockTotal = carrito[1].stockTotal - CantidadAgregarAlCarro;
+            carrito[1].enCarro = true;
+            carrito[1].stockEnCarro = carrito[1].stockEnCarro + CantidadAgregarAlCarro;
+            // carrito[1].dineroTotalEnCarro = carrito[1].precio * carrito[1].stockEnCarro;
+        } 
+        else {
+            console.log('Lo sentimos no queda suficiente stock de este producto');
+        }
+    } else if (agregarAlCarro == 3) {
+        if ((carrito[2].stockTotal > 2) && (carrito[2].stockTotal >= CantidadAgregarAlCarro)) {
+            carrito[2].stockTotal = carrito[2].stockTotal - CantidadAgregarAlCarro;
+            carrito[2].enCarro = true;
+            carrito[2].stockEnCarro = carrito[2].stockEnCarro + CantidadAgregarAlCarro;
+            // carrito[2].dineroTotalEnCarro = carrito[2].precio * carrito[2].stockEnCarro;
+        }
+        else {
+            console.log('Lo sentimos no queda suficiente stock de este producto');
+        }
+    } else {
+        console.log('EROrr');
+    }
+
+    let aux = prompt('Ingrese un 1 para SALIR, Ingrese cualquier tecla para seguir agregando items al carrito.');
+    if (aux == '1') {
+        flag = true;
+    }
+}
+
+
+//000000000000000000000000000000000000000000000000000000000000000000
+let vtc;
+for (let i of carrito) {
+    vtc = i.valorDeMercaderiaEnCarro();
+    totalCarrito = totalCarrito + vtc;
+} 
+console.log('Su total del carrito es de ' + totalCarrito);
+
+
 
 valorDescuento = 15; //15% con el codigo coder
 palabraDescuento = prompt('Ingrese el codigo de descuento: ');
@@ -65,11 +150,3 @@ if (palabraCuotas == 'si') {
 } else {
     console.log('No eligio cuotas, asi que su total permanece en '+ totalCarrito);
 }
-
-
-
-
-
-
-
-
