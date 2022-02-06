1dc9cc2e1f719e08697c5e00691cb82d41bb2b02
agusdellaquila <agusdellaquila72@gmail.com>
2022-02-06 00:51:26 -0300

    agregados eventos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-//objetos
+//-------------------------------------------------------
+//----creacion de objetos--------------------------------
+//-------------------------------------------------------
 class Prenda {
     constructor(id, nombrePrenda, precioPrenda, stockTotalDePrenda) {
         //propiedades del objeto
@@ -19,7 +21,9 @@ class PrendaEnCarro {
     }
 }
 
-/* Creo un array con los productos, como si fuera un .json */
+//-------------------------------------------------------
+//----equivalente temp al .json--------------------------
+//-------------------------------------------------------
 let IngresoProductos = [];
 
 IngresoProductos[0] = {id: '1001', nombrePrenda: 'Cobra Venom', precioPrenda: 8000, stockTotalDePrenda: 10};
@@ -31,67 +35,80 @@ IngresoProductos[5] = {id: '6', nombrePrenda: 'D1srupted nr3', precioPrenda: 310
 IngresoProductos[6] = {id: '7', nombrePrenda: 'Blue Denim lake', precioPrenda: 2750, stockTotalDePrenda: 60};
 IngresoProductos[7] = {id: '8', nombrePrenda: 'Blue Denim nr2', precioPrenda: 3500, stockTotalDePrenda: 400};
 
-//-------------------declaro array de prendas-------------------------
+//-------------------------------------------------------
+//-----------declaro array de prendas--------------------
+//-------------------------------------------------------
 let arrayPrendas = [];
 arrayPrendas = IngresoProductos.map((item) => new Prenda(item.id, item.nombrePrenda, item.precioPrenda, item.stockTotalDePrenda));
 
-//-------------------declaro array de carrito-------------------------
+//-------------------------------------------------------
+//-----------declaro array de carrito--------------------
+//-------------------------------------------------------
 let carrito = [];
 
-// ESTO SE VA A HACER CON EVENTOS, al tocar la img del producto, se agrega al carrito
+//Parte de la tarea de usar un event on ENTER key
+let inputPromocode = document.getElementById('promocodeInput');
 
-// let nombreID = captureInput();
+let pCorrecto = document.getElementById('pEnter');
+inputPromocode.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        if (inputPromocode.value == 'venenous') {
+            pCorrecto.innerHTML = "Codigo correcto";
+        } else pCorrecto.innerHTML = "Codigo INCORRECTO";
+    }
+});
+//------------------------------------------------
 
-let flag = false;
-//ahora las etradas de datos son con prompt pero dejaran de serlo y pasara a ser eventos
+let btnRestar = document.getElementById('restar');
+let btnSumar = document.getElementById('sumar');
+let cantidadEntrante = document.getElementById('ingresoDeCantidad');
 
-let nombreID = prompt('Ingrese ID del item para ingresar al carro (IDs: 1001-1002-1003): ');
-let cantidadEntrante = Number(prompt('Ingrese la cantidad de items que deseea agregar: '));
+btnRestar.addEventListener('click', () => {
+    subtractAmount(cantidadEntrante);
+});
 
-let prod = findProduct(arrayPrendas, nombreID);
+btnSumar.addEventListener('click', () => {
+    addAmount(cantidadEntrante);
+});
 
-if (prod == undefined) {
-    wrongInput();
-} else {
-    if (checkStock(prod, cantidadEntrante)) {
-        manageStock(prod, cantidadEntrante);
-        addToCart(carrito, prod, cantidadEntrante);
+//-------------------------------------------------------
+//------------------- funciones -------------------------
+//-------------------------------------------------------
+//--------- logic caller ------------
+function btnToCart() {
+    let nombreID = document.getElementById('ingresoDeId').value;
+    let cantidadFinal = parseInt(document.getElementById('ingresoDeCantidad').value);
+    
+    let prod = findProduct(arrayPrendas, nombreID);
+    
+    if (prod == undefined) {
+        wrongInput();
     } else {
-        noStock();
+        if (checkStock(prod, cantidadFinal)) {
+            manageStock(prod, cantidadFinal);
+            addToCart(carrito, prod, cantidadFinal);
+
+            //---------calcular precio total del carro------
+            let cartTotal = totalCartPrice(carrito);
+
+            //---------Mostrar items en carro------
+            let productoDelCarro = findProduct(carrito, nombreID);
+            showItemOnCart(productoDelCarro);
+
+            //---------Mostrar burbuja en carro------
+            amountItemsInCart(carrito);
+
+            //calcular descuento si es necesario
+            cartTotal = applyDiscount(cartTotal);
+            
+            //---------Mostrar precio total del carro------
+            showTotalPrice(cartTotal);
+        } else {
+            noStock();
+        }
     }
 }
-
-//mostrar items del carro
-let productoDelCarro = findProduct(carrito, nombreID);
-showItemOnCart(productoDelCarro);
-
-//precio total del carro
-let cartTotal = totalCartPrice(carrito);
-
-//aplico descuento si es necesario
-let inputPromocode = document.getElementById('promocodeInput').value
-let desc = 35;
-if (inputPromocode == 'venenous') {
-    cartTotal = applyDiscount(cartTotal, desc);
-}
-//muestro el total del carro
-let showTotalPrice = document.getElementById('cartTotalPrice');
-showTotalPrice.innerText = cartTotal;
-
-//BUBBLE cantidad de items en el carro
-document.getElementById('numOfItemsInCart').innerHTML = amountItemsInCart(carrito);
-//---------------------------
-
-
-
-//----------------- Funciones ----------------
-//!!!!!!!!!!!!!!!!!!HACER: btn to cart, agarrar el id del boton y pasarlo como parametro
-
-function captureInput() {
-    let input = document.getElementById('ingresoDeId').value;
-    return input
-}
-
+//---------Main set of functions------
 function findProduct(array, id) {
     //searches prodcut by id on arrayPrendas and returns the item
     for (item of array) {
@@ -100,63 +117,111 @@ function findProduct(array, id) {
         }
     }
 }
-
 function checkStock(item, amount) {
     //checks if there is stock for the set amount on the given item
     if (amount <= item.stockTotalDePrenda) {
         return true
     }
 }
-
 function manageStock(item, amount) {
     item.stockTotalDePrenda -= amount;
 }
-
-function noStock() {
-    let showName = document.getElementById('cartProduct1');
-    showName.innerText = 'No hay stock suficiente de este producto';
-}
-
-function wrongInput() {
-    let showName = document.getElementById('cartProduct1');
-    showName.innerText = 'Error al ingresar los datos';
-}
-
 function addToCart(array, item, amount) {
     array.push(new PrendaEnCarro(item.id, item.nombrePrenda, item.precioPrenda, amount));
 }
-
-function showItemOnCart(item) {
-    let showName = document.getElementById('cartProduct1');
-    let showAmount = document.getElementById('cartProductAmount1');
-    let showPrice = document.getElementById('cartPrice1');
-
-    showName.innerText = item.nombrePrenda;
-    showAmount.innerText = 'x' + item.stockDePrendaEnCarro;
-    showPrice.innerText = item.precioPrenda * item.stockDePrendaEnCarro;
-}
-
 function totalCartPrice(array) {
-    let total;
+    let total = 0;
     for (item of array) {
         total = total + (item.precioPrenda * item.stockDePrendaEnCarro);
     }
 
     return total
 }
+function applyDiscount(total) {
+    let keyword = document.getElementById('promocodeInput').value;
+    let desc = 35;
+    let newtotal;
+    if (keyword == 'venenous') {
+        newtotal = total - ((total * desc) / 100);
+        return newtotal;
+    } else return total
+}
+//---------Add amount functions------
+function addAmount(input) {
+    input.value = parseInt(input.value) + 1;
+}
+function subtractAmount(input) {
+    input.value = parseInt(input.value) - 1;
+}
+//---------Borrar carrito------
+function ClearCart(arrayCarro, arrayProd) {
+    for (element of arrayCarro) {
+        returnStockBack(arrayProd, element.id, element.stockDePrendaEnCarro);
+        arrayCarro.shift()
+    }
+    showItemOnCart();
+    showTotalPrice();
+    amountItemsInCart();
+    
+}
+function returnStockBack(arrayProd, idToReturn, amountToReturn) {
+    let i = 0;
+    while (true) {
+        if (arrayProd[i].id == idToReturn) {
+            arrayProd[i].stockTotalDePrenda += amountToReturn;
+            break;
+        }
+    }
+}
+//---------errors & warnings------
+function noStock() {
+    let showName = document.getElementById('cartProduct1');
+    showName.innerText = 'No hay stock suficiente de este producto';
+}
+function wrongInput() {
+    let showName = document.getElementById('cartProduct1');
+    showName.innerText = 'Error al ingresar los datos';
+}
+//---------Prints, show on screen------
+function showItemOnCart(item) {
+    let showName = document.getElementById('cartProduct1');
+    let showAmount = document.getElementById('cartProductAmount1');
+    let showPrice = document.getElementById('cartPrice1');
 
+    if (item !== undefined) {
+        showName.innerText = item.nombrePrenda;
+        showAmount.innerText = 'x' + item.stockDePrendaEnCarro;
+        showPrice.innerText = item.precioPrenda * item.stockDePrendaEnCarro;
+    } else {
+        showName.innerText = 'Empty';
+        showAmount.innerText = '';
+        showPrice.innerText = '';
+    }
+}
 function amountItemsInCart(array) {
-    return array.length
+    let bubble = document.getElementById('numOfItemsInCart')
+    let total = 0;
+    if (array !== undefined) {
+        for (element of array) {
+            total = total + element.stockDePrendaEnCarro;
+        }
+        bubble.innerHTML = total;
+    } else bubble.innerHTML = ''
+}
+function showTotalPrice(total) {
+    let showTotal = document.getElementById('cartTotalPrice');
+    if (total !== undefined) {
+        showTotal.innerText = total;
+    } else {
+        showTotal.innerText = '';
+    }
 }
 
 
 
-//masssssssssssssssssssssssssssssssss
-function applyDiscount(total, descuento) {
-    total = total - ((total * descuento) / 100);
-    return total;
-}
 
+
+//Estas funciones estan creadas pero no usadas de momento
 function applyTaxes(total) {
     let tax = 21;
     total = total + ((total * tax) / 100)
